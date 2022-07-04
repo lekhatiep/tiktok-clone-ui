5d8098820b70efc5a2cc33aeb186f8e4ecdc7bd8
tieplk <tieplk@dev.ndex.net>
2022-07-04 18:20:31 +0700

    config router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import { publicRoutes } from '~/routes';
+
 function App() {
   return (
-    <div className="App">
-      <h1>App react</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Page = route.component;
+            return <Route key={index} path={route.path} element={<Page />} />;
+          })}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
